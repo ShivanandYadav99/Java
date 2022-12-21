e310080b6938b189a15ac3af528267f9691baf13
Shivanand Yadav <111229426+ShivanandYadav99@users.noreply.github.com>
2022-12-21 19:40:09 +0530

    Print a, c, e...

---
+++ Basic Java/Print a, c, e... till the characters are less than z.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       // for(int i=0;i<26;i=i+2){
+       //     System.out.println((char)(i+'a'));
+      //  }
+        
+        for(char ch='a';ch<='z';ch+=2){
+            System.out.println(ch);
+        }
+    }
+}
+    
