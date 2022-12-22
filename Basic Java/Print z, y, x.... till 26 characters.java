fda55c7146fb302e0d6d25bedc639d523d896938
Shivanand Yadav <111229426+ShivanandYadav99@users.noreply.github.com>
2022-12-22 18:56:33 +0530

    Print z, y, x.... till 26 characters

---
+++ Basic Java/Print z, y, x.... till 26 characters.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         for(char ch='z';ch>='a';ch--){
+           System.out.println(ch);
+       //    char ch='z';
+       //  for(int i=26;i>0;i--){
+       //     System.out.println(ch);
+       //      ch--;
+         }
+        
+    }
+}
